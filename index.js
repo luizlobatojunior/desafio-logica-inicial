bcce517a22503e11afc73237bb4d354edb821ff4
luizlobatojunior <luizlobatojr@gmail.com>
2024-04-25 00:03:43 +0100

    atualizando desafio

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Ola mundão")
+// Definindo o nome e a quantidade de experiência do herói
+var nomeDoHeroi = "Miguel";
+var xpDoHeroi = 5000; // Altere o valor da XP conforme necessário
+
+// Utilizando uma estrutura de decisão para determinar o nível do herói
+var nivelDoHeroi;
+
+if (xpDoHeroi < 1000) {
+    nivelDoHeroi = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivelDoHeroi = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    nivelDoHeroi = "Prata";
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000) {
+    nivelDoHeroi = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+    nivelDoHeroi = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    nivelDoHeroi = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    nivelDoHeroi = "Imortal";
+} else {
+    nivelDoHeroi = "Radiante";
+}
+
+// Exibindo a mensagem com o nome e o nível do herói
+console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi);
